f79aa65b567cacbf05f79cfd23eeef58de33946a
Guilherme Alves Pimenta <guialvespimenta27@gmail.com>
2024-04-07 18:43:11 -0300

    readding layers

--- infra/services/__init__.py
+++ infra/services/__init__.py
@@ -1,3 +1,4 @@
+from infra.services.layers import Layers
 from infra.services.api_gateway import APIGateway
 from infra.services.aws_lambda import AWSLambda
 
@@ -6,3 +7,4 @@ class Services:
     def __init__(self, scope, context) -> None:
         self.api_gateway = APIGateway(scope, context)
         self.aws_lambda = AWSLambda(scope, context)
+        self.layers = Layers(scope)
